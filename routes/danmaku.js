c69625b32a9fa68bbdb6c735b5fe24ac69a4fd65
lyz05 <294068487@qq.com>
2024-01-08 12:11:55 +0800

    feat: add limit

--- routes/danmaku.js
+++ routes/danmaku.js
@@ -18,8 +18,8 @@ const rateLimit = require('express-rate-limit');
 const MAX_count_today = 1000;
 const allowlist = ['::1', '::ffff:127.0.0.1'];
 const apiLimiter = rateLimit({
-	windowMs: 5 * 60 * 1000, // 5 minutes
-	max: 20, // limit each IP to 20 requests per windowMs
+	windowMs: 2 * 60 * 1000, // 2 minutes
+	max: 10, // limit each IP to 10 requests per windowMs
 	message: 'Too many requests from this IP, please try again later',
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	skipFailedRequests: true, // Don't count failed requests (status >= 400)
